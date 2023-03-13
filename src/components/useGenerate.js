34c561fd64bff45fe2b938b7365668e3d763278f
Clement <clementlee1999@gmail.com>
2023-03-14 00:13:47 +0800

    declared constants

--- src/components/useGenerate.js
+++ src/components/useGenerate.js
@@ -1,9 +1,24 @@
 const useGenerate = () => {
 
-    const alphabet = Array.from({length: 26}, (_, i) => String.fromCharCode('a'.charCodeAt(0) + i));
-    console.log(alphabet);
+    const uppercase_alphabets = String.fromCharCode(...Array.from(new Array(26), (_, i) => 65 + i))
+    const lowercase_alphabets = String.fromCharCode(...Array.from(new Array(26), (_, i) => 97 + i))
+    const numbers = String.fromCharCode(...Array.from(new Array(10), (_, i) => 48 + i))
+    const symbols = "!@#$%^&*()_+-={}[]|\\:;\"'<>,.?/"
+
+    console.log(uppercase_alphabets)
+    console.log(lowercase_alphabets)
+    console.log(numbers)
+    console.log(symbols)
 
 
+
+
+
+
+
+
+
+    
 }
  
 export default useGenerate;
